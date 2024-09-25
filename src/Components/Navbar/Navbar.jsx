286142187303acf432f4cb7e06eea02a485bb877
wacho91 <criskol.71@gmail.com>
2024-09-25 11:02:21 -0500

    The Section login & register

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -34,6 +34,10 @@ const Navbar = () => {
                     </ul>
                 </div>
                 {/*CTA Button Section*/}
+                <div className="hidden lg:block space-x-6">
+                    <button className="font-semibold">Sign In</button>
+                    <button className="text-white bg-secondary font-semibold rounded-full px-6 py-2">Register</button>
+                </div>
                 {/*Mobile Hamburger Menu*/}
             </div>
         </nav>
