216c0d4db1ae7d5d474716a1c43830a7e649a999
wacho91 <criskol.71@gmail.com>
2024-09-26 10:51:06 -0500

    Hero Component Complete

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,8 @@
 
+import { FaPlay } from "react-icons/fa"
 import HeroImg from "../../Assets/hero.png"
+import { motion } from "framer-motion"
+import { SlideRight } from "../../Utiliy/Animation"
 
 const Hero = () => {
   return (
@@ -7,20 +10,57 @@ const Hero = () => {
       {/*Brand Info*/}
       <div className="flex flex-col justify-center py-14 md:pr-16 xl:pr-40 md:py-0">
         <div className="text-center md:text-left space-y-6">
-            <p className="text-orange-600 uppercase font-semibold">100% Satistaction Guarantee</p>
-            <h1>Find your Perfect Tutor</h1>
-            <p>
+            <motion.p
+                variants={SlideRight(0.4)}
+                initial="hidden"
+                animate="visible" 
+                className="text-orange-600 uppercase font-semibold"
+            >
+                100% Satistaction Guarantee
+            </motion.p>
+            <motion.h1
+                variants={SlideRight(0.6)}
+                initial="hidden"
+                animate="visible" 
+                className="text-5xl font-semibold lg:text-6xl !leading-tight"
+            >
+                Find your Perfect <span className="text-orange-600">Tutor</span>
+            </motion.h1>
+            <motion.p
+                variants={SlideRight(0.8)}
+                initial="hidden"
+                animate="visible"
+            >
                 We help find perfect tutor for 1-on-1 lessons.
                 It is completely free and private
-            </p>
+            </motion.p>
             {/*Button Section*/}
-            <button>Get Started</button>
-            <button>See how it works</button>
+            <motion.div
+                variants={SlideRight(1.0)}
+                initial="hidden"
+                animate="visible" 
+                className="flex gap-8 justify-center md:justify-start !mt-8 items-center"
+            >
+                <button className="primary-btn">Get Started</button>
+                <button className="flex justify-end items-center gap-2 font-semibold">
+                    <span className="w-10 h-10 bg-secondary/15 rounded-full flex justify-center items-center">
+                        <FaPlay className="text-secondary" />
+                    </span>
+                    See how it works
+                </button>
+            </motion.div>
         </div>
       </div>
       {/*Hero Image*/}
-      <div>
-        <img src={HeroImg} alt="" />
+      <div className="flex justify-center items-center">
+        <motion.img 
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ type: "spring", stiffness: 100, delay: 0.2 }}
+            src={HeroImg} 
+            alt="" 
+            className="w-[350px] md:w-[550px] xl:w-[700px]" 
+        />
       </div>
     </div>
   )
