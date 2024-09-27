a9e513e82cbe36b848e1c255986f565b6279dae0
wacho91 <criskol.71@gmail.com>
2024-09-27 10:09:02 -0500

    Banner Complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,27 @@ import NavbarBanner from "./Components/Navbar/NavbarBanner"
 import NumberCounter from "./Components/NumberCounter/NumberCounter"
 import WhyChooseUs from "./Components/WhyChooseUs/WhyChooseUs"
 
+import Img1 from "./assets/banner1.png";
+import Img2 from "./assets/banner2.png";
+
+const BannerDate = {
+  image: Img1,
+  tag: "CUSTOMIZE WITH YOUR SCHEDULE",
+  title: "Personalized Professional Online Tutor on Your Schedule",
+  subtitle:
+    "Our scheduling system allows you to select based on your free time. Lorem ipsum demo text for template. Keep track of your students class and tutoring schedules, and never miss your lectures. The best online class scheduling system with easy accessibility.Lorem ipsum is a placeholder text commonly used to demonstrate the visual form",
+  link: "#",
+}
+
+const BannerData2 = {
+  image: Img2,
+  tag: "CUSTOMIZE WITH YOUR SCHEDULE",
+  title: "Talented and Qualified Tutors to Serve You for Help",
+  subtitle:
+    "Our scheduling system allows you to select based on your free time. Lorem ipsum demo text for template. Keep track of your students class and tutoring schedules, and never miss your lectures. The best online class scheduling system with easy accessibility. Lorem ipsum is a placeholder text commonly used",
+  link: "#",
+};
+
 function App() {
   
 
@@ -15,7 +36,8 @@ function App() {
       <Hero />
       <NumberCounter />
       <WhyChooseUs />
-      <Banner />
+      <Banner {...BannerDate} />
+      <Banner {...BannerData2} reverse={true} />
     </main>
   )
 }
